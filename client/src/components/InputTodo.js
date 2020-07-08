116d0066b5e0e5a08a93f4817a2ea9d9bd532c72
soydavido <ortuod@gmail.com>
2020-07-08 16:08:11 -0400

    Haciendo pruebas

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,12 @@
+import React, {Fragment, useState} from "react"
+
+const InputTodo = () =>{
+
+    return (
+       <Fragment>
+           <h1>Sidebar</h1>
+       </Fragment>
+    )
+};
+
+export default InputTodo
---
+++ client/src/components/barraLateral.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const barraLateral = () => {
+    return (
+        <h1>Hola Mundo</h1>
+    )
+};
+
+export default barraLateral;
