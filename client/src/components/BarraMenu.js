36c9902c77f4fc9e04765234153c1bf0e31346b9
soydavido <ortuod@gmail.com>
2020-07-09 16:44:36 -0400

    Test

---
+++ client/src/components/BarraMenu.js
@@ -0,0 +1,15 @@
+import "./../style.css";
+import React,{ Fragment, useState } from "react";
+
+const BarraMenu = () => {
+    return  (
+        
+        <nav class="sidebar">
+            <div class="sidebar-header">
+                <h3 className="text center mt-3 ml-3">Adomatics</h3>
+            </div>
+        </nav>
+    )
+};
+
+export default BarraMenu;
