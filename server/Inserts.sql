483dd38ceb7fcef5a6ef32fd02b1c893cd5b8608
soydavido <ortuod@gmail.com>
2020-07-22 14:49:41 -0400

    .

--- server/Inserts.sql
+++ server/Inserts.sql
@@ -1,5 +1,6 @@
 insert into add_productores (prod_id,prod_nombre,prod_email,prod_pag_web,prod_direccion,prod_id_aso_nac) values 
-(1,'Firmenich','information@firmenich.com','www.firmenich.com/','Rue de la Bergère 7, 1217 Meyrin, Suiza',null),
-(2,'Symrise','bernhard.kott@symrise.com','www.symrise.com/','Muhlenfeldstrasse 1, Holzminden, Lower Saxony 37603 DE',null);
+(1,'Symrise','bernhard.kott@symrise.com','www.symrise.com/','Muhlenfeldstrasse 1, Holzminden, Lower Saxony 37603 DE',null),
+(2,'Privi','info@privi.co.in','www.privi.com/','Privi House, A-71,TTC Industrial Area,Thane Belapur Road,Kopar Khairane,Navi Mumbai - 400 709, India',null);
 
-
+insert into add_proveedores (prov_id,prov_nombre,prov_email,prov_pag_web,prov_direccion,prov_id_aso_nac) values 
+(1,'Firmenich','information@firmenich.com','www.firmenich.com/','Rue de la Bergère 7, 1217 Meyrin, Suiza',null);
