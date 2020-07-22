d115ae2ca13b90f9e253d3bb81920ff76fe9ece3
soydavido <ortuod@gmail.com>
2020-07-22 14:58:45 -0400

    paises + productores + proveedores iniciales

--- server/Inserts.sql
+++ server/Inserts.sql
@@ -1,6 +1,32 @@
-insert into add_productores (prod_id,prod_nombre,prod_email,prod_pag_web,prod_direccion,prod_id_aso_nac) values 
-(1,'Symrise','bernhard.kott@symrise.com','www.symrise.com/','Muhlenfeldstrasse 1, Holzminden, Lower Saxony 37603 DE',null),
-(2,'Privi','info@privi.co.in','www.privi.com/','Privi House, A-71,TTC Industrial Area,Thane Belapur Road,Kopar Khairane,Navi Mumbai - 400 709, India',null);
+insert into add_paises (pai_nombre) values 
+	('India'),
+	('Grecia'),
+	('Australia'),
+	('Nueva Zelanda'),
+	('China'),
+	('Indoniesia'),
+	('Japon'),
+	('Korea'),
+	('Singapur'),
+	('Francia'),
+	('Alemania'),
+	('Italia'),
+	('Holanda'),
+	('España'),
+	('Suiza'),
+	('Turquia'),
+	('Reino Unido'),
+	('Argentina'),
+	('Brazil'),
+	('Chile'),
+	('Colombia'),
+	('Mexico'),
+	('Canada'),
+	('Estados Unidos');
 
-insert into add_proveedores (prov_id,prov_nombre,prov_email,prov_pag_web,prov_direccion,prov_id_aso_nac) values 
-(1,'Firmenich','information@firmenich.com','www.firmenich.com/','Rue de la Bergère 7, 1217 Meyrin, Suiza',null);
+insert into add_productores (prod_nombre,prod_email,prod_pag_web,prod_direccion,prod_id_aso_nac) values 
+('Symrise','bernhard.kott@symrise.com','www.symrise.com/','Muhlenfeldstrasse 1, Holzminden, Lower Saxony 37603 DE',null);
+
+insert into add_proveedores (prov_nombre,prov_email,prov_pag_web,prov_direccion,prov_id_aso_nac,pro_id_pai) values 
+('Firmenich','information@firmenich.com','www.firmenich.com/','Rue de la Bergère 7, 1217 Meyrin, Suiza',null,15),
+('Privi','info@privi.co.in','www.privi.com/','Privi House, A-71,TTC Industrial Area,Thane Belapur Road,Kopar Khairane,Navi Mumbai - 400 709, India',null,1);
