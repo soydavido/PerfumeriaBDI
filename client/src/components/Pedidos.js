2babc72304157df41d8dbe5c343d0fa481f7fcdc
soydavido <ortuod@gmail.com>
2020-07-30 12:52:04 -0400

    Creacion pedidos.js

---
+++ client/src/components/Pedidos.js
@@ -0,0 +1,14 @@
+import "./../style.css";
+import Inicio from "./Inicio";
+import React,{ Fragment, useState, useEffect } from "react";
+import Dropdown from "./Dropdown";
+import styles from "../style.css"
+import InputFuncional from "./InputFuncional";
+
+
+
+class Pedidos extends React.Component{
+
+}
+
+export default Pedidos;
